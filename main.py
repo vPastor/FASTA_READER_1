91543700602ce1f4ecc5028042f4c713a048ddf6
vPastor <43339640+vPastor@users.noreply.github.com>
2018-09-19 19:35:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-
+import sys
+if len(sys.argv)>2:
+	print ('Numero de argumentos invalidos')
+(sys.argv)
 field = '/home/victor/Desktop/pdb_seq.txt'
 for  linea  in  open(field):
-	if '>' in linea:
+	if (('>') == (inea.split()[0)):
 		print  (linea)
 
